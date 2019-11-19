0d4f8127316ca0140661a53dd94b6808346825e1
bbster <bbster12@naver.com>
2019-11-20 01:26:28 +0900

    commit

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,14 @@
 print("안녕하세요?")
 name=input("이름을 입력하세요: ")
-print("입력한 이름은{s}입니다" .format(s=name))
+print("입력한 이름은 {s}입니다" .format(s=name))
+print("입력한 이름은 {}입니다" .format(name))
+print("입력한 이름은 %s입니다" %(name))
+#--------------------------------------#
+
+print("123")
+print("Hello!")
+print("")
+print("a")
+print(" ")
+print("I'm a good boy.")
+print('저 "반달곰"은 어디에서 왔을까?')
