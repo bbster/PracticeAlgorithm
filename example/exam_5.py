6b9bc7f3271cfcfcb6e71c317df72129cea6e65f
bbster <bbster12@naver.com>
2019-11-22 01:49:58 +0900

    exam_5

---
+++ example/exam_5.py
@@ -0,0 +1,11 @@
+import math
+
+num=9.5
+
+#정수형으로 출력
+print("9.5의 반올림 값 = %d" %(math.ceil(num)))  #올림값
+print("9.5의 반올림 값 = %d" %(math.floor(num)))  #내림값
+
+#실수형으로 출력
+print("9.5의 반올림 값 = %f" %(math.ceil(num)))  #올림값
+print("9.5의 반올림 값 = %f" %(math.floor(num)))  #내림값
