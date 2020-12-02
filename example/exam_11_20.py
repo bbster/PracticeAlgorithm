74e49170f02d913304a0d655643c6121fd17ea33
bbster <bbster12@naver.com>
2020-12-02 14:16:39 +0900

    exam 11-20

---
+++ example/exam_11_20.py
@@ -0,0 +1,70 @@
+# https://wikidocs.net/7014
+
+# 011
+삼성전자 = 50000
+print("평가금액", 삼성전자 * 10)
+
+# 012
+시가총액 = 298000000000
+현재가 = 50000
+PER = 15.79
+
+print("시가총액:", 시가총액, "현재가:", 현재가, "PER:", PER)
+
+# 답안지
+# 시가총액 = 298000000000000
+# 현재가 = 5000
+# PER = 15.79
+# print(시가총액, type(시가총액))
+# print(현재가, type(현재가))
+# print(PER, type(PER))
+# type(변수) - 변수의 데이터 타입을 알수있다. int형인지 float인지 등등
+
+# 013
+s = "hello"
+t = "python"
+
+print(s, end="! ");print(t)
+
+# 답안지
+# s = "hello"
+# t = "python"
+# print(s+"!", t)
+
+# 014
+print(2+2*3)
+
+# 015
+a = "128"
+print(type(a))
+# class 'str'
+
+# 016
+num_str = "720"
+num_int_casting = int("720")
+print(num_str, type(num_str))
+print(num_int_casting, type(num_int_casting))
+
+# 017
+num = 100
+str_casting = str(100)
+str_casting2 = str(num)
+
+print(str_casting, type(str_casting))
+print(str_casting2, type(str_casting2))
+
+# 018
+str_a = "15.79"
+float_casting = float(str_a)
+print(float_casting, type(float_casting))
+
+# 019
+year = "2020"
+print(year, type(year))
+year_int_casting = int(year)
+print(year_int_casting, type(year_int_casting))
+
+# 020
+air_conditioner = 48584
+term = 36
+print(air_conditioner * term)
