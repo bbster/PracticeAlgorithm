53520b8290d9df188a3b83dc7dfbf1841cf0742f
bbster <bbster12@naver.com>
2020-12-02 12:54:57 +0900

    exam 1-10 clear

---
+++ example/exam_1_10.py
@@ -0,0 +1,32 @@
+# https://wikidocs.net/7014
+
+# 001
+print("Hello World")
+
+# 002
+print("Mary's cosmetics")
+
+# 003
+print('신씨가 소리질렀다. "도둑이야".')
+
+# 004
+print('"c:\Windows"')
+
+# 005
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+# \n 줄바꿈 \t 탭
+
+# 006
+print("오늘은", "일요일")
+
+# 007
+print("naver", "kakao", "sk", "samsung", sep=";")
+
+# 008
+print("naver", "kakao", "sk", "samsung", sep="/")
+
+# 009
+print("first", end="");print("second")
+
+# 010
+print(5/3)
