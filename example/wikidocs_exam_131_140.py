eb6070f9bf565b8d9056bfed951389af87def2d2
bbster <bbster12@naver.com>
2020-12-09 19:33:33 +0900

    '131-140'

---
+++ example/wikidocs_exam_131_140.py
@@ -0,0 +1,69 @@
+# https://wikidocs.net/7014
+
+# 131
+과일 = ["사과", "귤", "수박"]
+for 변수 in 과일:
+    print(변수)
+print()
+
+
+# 132
+과일 = ["사과", "귤", "수박"]
+for 변수 in 과일:
+  print("#####")
+print()
+
+# 133
+for 변수 in ["A", "B", "C"]:
+  print(변수)
+print()
+
+# 134
+for 변수 in ["A", "B", "C"]:
+  print("출력:", 변수)
+print()
+
+# 135
+for 변수 in ["A", "B", "C"]:
+  b = 변수.lower()
+  print("변환:", b)
+
+변수 = "A"
+b = 변수.lower()
+print("변환:", b)
+변수 = "B"
+b = 변수.lower()
+print("변환:", b)
+변수 = "C"
+b = 변수.lower()
+print("변환:", b)
+print()
+
+# 136
+num_list = [10, 20, 30]
+for i in num_list:
+  print(i)
+print()
+
+# 137
+num_list = [10, 20, 30]
+for i in num_list:
+  print(i)
+print()
+
+# 138
+num_list = [10, 20, 30]
+for i in num_list:
+  print(i)
+  print('------')
+print()
+# 139
+num_list = [10, 20, 30]
+print('+++')
+for i in num_list:
+  print(i)
+print()
+
+# 140
+for i in range(0, 4):
+    print('----')
